480f6553e86f37a8108022625d18b5c1d4958667
kovidd <49589399+kovidd@users.noreply.github.com>
2021-03-09 18:19:35 +1100

    Update data_load.sql

--- SQL/data_load.sql
+++ SQL/data_load.sql
@@ -58,47 +58,17 @@ copy trip_fare_raw FROM 'D:/trip_fare_2_3.csv' DELIMITER ',' CSV; -- 4663391
 SELECT * FROM trip_data_raw LIMIT 5;
 SELECT * FROM trip_fare_raw LIMIT 5;
 
-
 -- Checking total number of records in both tables, should be 13990176
 SELECT COUNT(*) FROM trip_data_raw; -- 13990176
 SELECT COUNT(*) FROM trip_fare_raw; -- 13990176
 
-
--- Counting distinct values for various attributes
-SELECT COUNT(DISTINCT (medallion)) FROM trip_data_raw; -- 13415
-SELECT COUNT(DISTINCT (hack_license)) FROM trip_data_raw; -- 32062
-SELECT COUNT(DISTINCT (medallion)) FROM trip_fare_raw; -- 13415
-SELECT COUNT(DISTINCT (hack_license)) FROM trip_fare_raw; -- 32062
-SELECT DISTINCT(vendor_id) FROM trip_data_raw; -- 2 ("VTS", "CMT")
-SELECT DISTINCT(rate_code)FROM trip_data_raw ORDER BY rate_code; -- 12 (0,1,2,3,4,5,6,7,28,79,210,221)
-SELECT DISTINCT(vendor_id) FROM trip_fare_raw; -- 2 ("VTS", "CMT")
-SELECT DISTINCT(payment_type)FROM trip_fare_raw ORDER BY payment_type; -- 5 ("CRD","CSH","DIS","NOC","UNK")
-
--- Unhasing the medallion/hack_license
-SELECT md5('5433881'); -- BA96DE419E711691B9445D6A6307C170
-
-
 -- Finding the composite key
 SELECT COUNT(DISTINCT (medallion,hack_license,vendor_id,pickup_datetime)) FROM trip_fare_raw; -- 13990176 SHOULD BE 13990176
+SELECT COUNT(DISTINCT (medallion,hack_license,pickup_datetime)) FROM trip_fare_raw; -- 13990176
 SELECT COUNT(DISTINCT (hack_license,vendor_id,pickup_datetime)) FROM trip_fare_raw; -- 13990025
 SELECT COUNT(DISTINCT (medallion,vendor_id,pickup_datetime)) FROM trip_fare_raw; -- 13990159
-SELECT COUNT(DISTINCT (medallion,hack_license,pickup_datetime)) FROM trip_fare_raw; -- 13990176
 SELECT COUNT(DISTINCT (medallion,pickup_datetime)) FROM trip_fare_raw; -- 13990159
 SELECT COUNT(DISTINCT (medallion,hack_license)) FROM trip_fare_raw; -- 95731
-
-
--- Make a master table with all fields
-SELECT d.medallion, d.hack_license, d.vendor_id, d.rate_code, d.store_and_fwd_flag, d.pickup_datetime, 
-	   d.dropoff_datetime, d.passenger_count, d.trip_time_in_secs, d.trip_distance, d.pickup_longitude, 
-	   d.pickup_latitude, d.dropoff_longitude, d.dropoff_latitude, f.payment_type, f.fare_amount, f.surcharge, 
-	   f.mta_tax, f.tip_amount, f.tolls_amount, f.total_amount
-INTO raw_consolidated
-FROM trip_data_raw d, trip_fare_raw f
-WHERE d.medallion=f.medallion AND
-	  d.hack_license=f.hack_license AND
-	  d.vendor_id=f.vendor_id AND
-	  d.pickup_datetime=f.pickup_datetime
-ORDER BY d.pickup_datetime, d.medallion, d.hack_license;
 
 
 -- Creating a master table
@@ -150,54 +120,77 @@ WHERE d.medallion=f.medallion AND
 ORDER BY d.pickup_datetime, d.medallion, d.hack_license;
 
 
--- Perform list deletion on raw_consolidated
+-- Perform list deletion on raw_master
 DELETE FROM raw_master
 WHERE trip_time_in_secs < 30 OR
 	  trip_distance < 0.5 OR
 	  ((trip_distance * 1.609 * 1000) / trip_time_in_secs) * 3.6 > 70 OR
-	  pickup_longitude = 0 AND pickup_latitude = 0 OR
-	  dropoff_longitude = 0 AND dropoff_latitude = 0 OR
-	  pickup_latitude NOT BETWEEN 39 AND 42 OR
-	  dropoff_latitude NOT BETWEEN 39 AND 42 OR
-	  pickup_longitude NOT BETWEEN -76 AND -72 OR
+	  pickup_latitude   NOT BETWEEN  39 AND  42 OR
+	  dropoff_latitude  NOT BETWEEN  39 AND  42 OR
+	  pickup_longitude  NOT BETWEEN -76 AND -72 OR
 	  dropoff_longitude NOT BETWEEN -76 AND -72 OR
-	  pickup_latitude = dropoff_latitude OR 
+	  pickup_latitude  = dropoff_latitude OR 
 	  pickup_longitude = dropoff_longitude
 ; -- 1052441
-	  
-SELECT COUNT(*) FROM raw_master; -- 12937735
 
 -- Resize columns based on data-range
 ALTER TABLE raw_master
-ALTER COLUMN trip_distance TYPE NUMERIC(5,2) USING ROUND(trip_distance * 1.609, 2),
-ALTER COLUMN pickup_longitude TYPE NUMERIC(7,4) USING ROUND(pickup_longitude, 4),
-ALTER COLUMN pickup_latitude TYPE NUMERIC(7,4) USING ROUND(pickup_latitude, 4),
-ALTER COLUMN dropoff_longitude TYPE NUMERIC(7,4) USING ROUND(dropoff_longitude, 4),
-ALTER COLUMN dropoff_latitude TYPE NUMERIC(7,4) USING ROUND(dropoff_latitude, 4),
-ALTER COLUMN fare_amount TYPE NUMERIC(5,2),
-ALTER COLUMN surcharge TYPE NUMERIC(4,2),
-ALTER COLUMN mta_tax TYPE NUMERIC(3,2),
-ALTER COLUMN tip_amount TYPE NUMERIC(5,2),
-ALTER COLUMN tolls_amount TYPE NUMERIC(4,2),
-ALTER COLUMN total_amount TYPE NUMERIC(5,2);
-
-SELECT * FROM raw_master 
-ORDER BY total_amount DESC
-LIMIT 1;
-
--- Release storage after row deletion
-VACUUM ANALYZE raw_master;
-
--- Describe table
-SELECT * FROM pg_stats WHERE tablename = 'raw_master';
+	ALTER COLUMN trip_distance TYPE NUMERIC(5,2) USING ROUND(trip_distance * 1.609, 2),
+	ALTER COLUMN pickup_longitude TYPE NUMERIC(7,4) USING ROUND(pickup_longitude, 4),
+	ALTER COLUMN pickup_latitude TYPE NUMERIC(7,4) USING ROUND(pickup_latitude, 4),
+	ALTER COLUMN dropoff_longitude TYPE NUMERIC(7,4) USING ROUND(dropoff_longitude, 4),
+	ALTER COLUMN dropoff_latitude TYPE NUMERIC(7,4) USING ROUND(dropoff_latitude, 4),
+	ALTER COLUMN fare_amount TYPE NUMERIC(5,2),
+	ALTER COLUMN surcharge TYPE NUMERIC(4,2),
+	ALTER COLUMN mta_tax TYPE NUMERIC(3,2),
+	ALTER COLUMN tip_amount TYPE NUMERIC(5,2),
+	ALTER COLUMN tolls_amount TYPE NUMERIC(4,2),
+	ALTER COLUMN total_amount TYPE NUMERIC(5,2);
 ---------------------------------------------------
 
--- Make tables for analysis
+------------------
+-- Rows deleted --
+------------------
+-- Slow (12 secs 127 msecs)
+WITH 
+	cte1 AS (SELECT COUNT(raw.*) AS total_records FROM trip_data_raw raw),
+	cte2 AS (SELECT COUNT(analysis.*) AS clean_records FROM raw_master analysis)
+SELECT total_records - clean_records AS deleted_records
+FROM cte1 c1, cte2 c2;
 
+-- Faster (3 secs 45 msecs)
+SELECT (SELECT COUNT(*) FROM trip_data_raw) - (SELECT COUNT(*) FROM raw_master) AS deleted_records; -- 1,052,441
+SELECT (SELECT COUNT(*) FROM trip_fare_raw) - (SELECT COUNT(*) FROM raw_master) AS deleted_records; -- 1,052,441
+
+---------------------------------------------------
+
+------------------------------
+-- Make tables for analysis --
+------------------------------
+
+-- Table 1
+DROP TABLE driver_analysis;
+
+-- medallion, hack_license, vendor
+SELECT id,
+	   medallion,
+	   hack_license,
+	   vendor_id,
+	   pickup_datetime
+INTO driver_analysis
+FROM raw_master
+ORDER BY id;
+
+SELECT * FROM driver_analysis LIMIT 5;
+---------------------------------------------------
+
+---------------------------------------------------
+
+-- Table 2
 DROP TABLE fare_analysis;
+
 -- time, fare
 SELECT id,
-	   vendor_id,
 	   pickup_datetime,
 	   CASE WHEN (trip_time_in_secs) <= 60  THEN 1 ELSE (trip_time_in_secs)/60 END AS trip_time_in_mins,
 	   payment_type,
@@ -210,13 +203,13 @@ FROM raw_master
 ORDER BY id;
 
 SELECT * FROM distance_time LIMIT 5;
-
 ---------------------------------------------------
 
+-- Table 3
 DROP TABLE data_analysis;
+
 -- for distance, geolocation
 SELECT id, 
-	   vendor_id, 
 	   pickup_datetime,
 	   dropoff_datetime,
 	   trip_time_in_secs,
@@ -235,5 +228,6 @@ ORDER BY id;
 ---------------------------------------------------
 
 -- Create csv from analysis tables
-COPY ( SELECT * FROM data_analysis ) TO 'D:/data_analysis.csv' With CSV DELIMITER ',' HEADER;
-COPY ( SELECT * FROM fare_analysis ) TO 'D:/fare_analysis.csv' With CSV DELIMITER ',' HEADER;
+COPY ( SELECT * FROM driver_analysis ) TO 'D:/data_analysis.csv' with CSV DELIMITER ',' HEADER;
+COPY ( SELECT * FROM data_analysis ) TO 'D:/data_analysis.csv' with CSV DELIMITER ',' HEADER;
+COPY ( SELECT * FROM fare_analysis ) TO 'D:/fare_analysis.csv' with CSV DELIMITER ',' HEADER;
